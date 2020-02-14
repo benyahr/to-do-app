76ac79b4b6b30fed89f5887e7e348c4a6d6f7df6
benyahr <57229997+benyahr@users.noreply.github.com>
2020-02-14 14:09:07 -0500

    Add files via upload
    updated javascript to include arrays

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 function onReady() {
+  let toDos = [];
+
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
@@ -8,6 +10,9 @@ function onReady() {
 
     // get the text
    let title = newToDoText.value;
+
+
+   toDos.push(title);
 
    // create a new li
    let newLi = document.createElement('li');
@@ -22,7 +27,15 @@ function onReady() {
 deleteBtn.textContent = "Delete";
 
 deleteBtn.addEventListener('click', function(event) {
-  toDoList.removeChild(this.parentElement)
+  let buttonLitext = this.parentElement.childNodes[0].textContent;
+  toDoList.removeChild(this.parentElement);
+
+  toDos.forEach(function(currentToDo, index) {
+    //console.log(currentToDo, index);
+    if(currentToDo === buttonLitext){
+      toDos.splice(index, 1);
+    }
+  });
 });
    // set the title
    newLi.textContent = title;
