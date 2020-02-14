6557bf842ed4f558a1f1f344b0587831266d16c0
benyahr <57229997+benyahr@users.noreply.github.com>
2020-02-14 13:23:52 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -18,11 +18,19 @@ function onReady() {
    // set the input's type to checkbox
    checkbox.type = "checkbox";
 
+   let deleteBtn = document.createElement('button');
+deleteBtn.textContent = "Delete";
+
+deleteBtn.addEventListener('click', function(event) {
+  toDoList.removeChild(this.parentElement)
+});
    // set the title
    newLi.textContent = title;
 
    // attach the checkbox to the li
    newLi.appendChild(checkbox);
+
+newli.appendChild(deleteBtn);
 
    // attach the li to the ul
    toDoList.appendChild(newLi);
